4f200127c087d82a2e13c0c182cfa2605d627950
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-17 04:38:57 -0400

    Add Lesson_27_Introduction_to_While_Loops/part_3_number_guessing_game.py

---
+++ Lesson_27_Introduction_to_While_Loops/part_3_number_guessing_game.py
@@ -0,0 +1,11 @@
+secret_number = 7
+guess = 0
+
+while guess != secret_number:
+    guess = int(input("Guess the number (between 1 and 10): "))
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+        
+print("Congratulations! You guessed the number!")
