f0300e42c49ba327f63c40ec0a819b6b65aadee3
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-17 04:36:31 -0400

    Add Lesson_13_Introduction_to_Programming_Concepts/part_2_variables_example.js

---
+++ Lesson_13_Introduction_to_Programming_Concepts/part_2_variables_example.js
@@ -0,0 +1,15 @@
+// Integer
+let apples = 5;
+
+// Floating-point
+let pricePerApple = 0.75;
+
+// Character
+let firstLetter = 'A';
+
+// String
+let message = "I have " + apples + " apples.";
+
+// Output the message
+console.log(message);
+console.log("Price per apple is: $" + pricePerApple);
